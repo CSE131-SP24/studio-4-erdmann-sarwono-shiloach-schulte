0574c2edead35f563e4376f7087744e7bc5d5786
izzyerdmann <izzyerdmann@localhost>
2024-02-21 14:17:38 -0600

    our flag is so winner

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,22 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+	StdDraw.setPenColor(149, 42, 251);
+	StdDraw.filledRectangle(0.5, 0.5, 0.425, 0.275);
+	StdDraw.setPenColor(186, 119, 251);
+	StdDraw.filledRectangle(0.5, 0.5, 0.4, 0.25);
+	StdDraw.setPenColor(240, 195, 17);
+	StdDraw.filledCircle(0.5, 0.5, 0.15);
+	StdDraw.setPenColor(13, 42, 100);
+	double []x= {0.1,0.4,0.22};
+	double []y= {0.25,0.25,0.4};
+	StdDraw.filledPolygon(x, y);
+	double []x1= {0.35,0.65,0.5};
+	double []y1= {0.25,0.25,0.5};
+	StdDraw.filledPolygon(x1, y1);
+	double []x2= {0.6,0.9,0.82};
+	double []y2= {0.25,0.25,0.4};
+	StdDraw.filledPolygon(x2, y2);
+
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,43 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shapeType = in.next();
+		int redComponent=in.nextInt();
+		int greenComponent=in.nextInt();
+		int blueComponent=in.nextInt();
+		boolean isFilled =in.nextBoolean();
+		
+		StdDraw.setPenColor(redComponent,greenComponent,blueComponent);
+		if(shapeType.equals("ellipse")) {
+			double comp1=in.nextDouble();
+			double comp2=in.nextDouble();
+			double comp3=in.nextDouble();
+			double comp4=in.nextDouble();
+			if(isFilled) {
+				StdDraw.filledEllipse(comp1,comp2,comp3,comp4);
+			}
+			else {
+				StdDraw.ellipse(comp1,comp2,comp3,comp4);
+			}
+		}
+		if(shapeType.equals("rectangle")) {
+			if(isFilled) {
+				StdDraw.filledRectangle(in.nextDouble(),in.nextDouble(),in.nextDouble(),in.nextDouble());
+			}
+			else {
+				StdDraw.rectangle(in.nextDouble(),in.nextDouble(),in.nextDouble(),in.nextDouble());
+			}
+		}
+		if(shapeType.equals("triangle")) {
+			double[]x = {in.nextDouble(),in.nextDouble(),in.nextDouble()};
+			double[]y = {in.nextDouble(),in.nextDouble(),in.nextDouble()};
+			if(isFilled) {
+				StdDraw.filledPolygon(x,y);
+			}
+			else {
+				StdDraw.polygon(x,y);
+			}
+		}
+		
 	}
 }
